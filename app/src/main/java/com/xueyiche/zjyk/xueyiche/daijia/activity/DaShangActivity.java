7697ef609112a273fd6ae60bcba5ce6c7b186d70
zhangsijia1993 <583822676@qq.com>
2022-04-08 09:12:22 +0800

    打赏2

--- app/src/main/java/com/xueyiche/zjyk/xueyiche/daijia/activity/DaShangActivity.java
+++ app/src/main/java/com/xueyiche/zjyk/xueyiche/daijia/activity/DaShangActivity.java
@@ -1,11 +1,21 @@
 package com.xueyiche.zjyk.xueyiche.daijia.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.xueyiche.zjyk.xueyiche.R;
 import com.xueyiche.zjyk.xueyiche.base.BaseActivity;
+import com.xueyiche.zjyk.xueyiche.utils.EditInputFilter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 打赏
@@ -13,6 +23,24 @@ import com.xueyiche.zjyk.xueyiche.base.BaseActivity;
 public class DaShangActivity extends BaseActivity {
 
 
+    @BindView(R.id.iv_common_back)
+    ImageView ivCommonBack;
+    @BindView(R.id.ll_common_back)
+    LinearLayout llCommonBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_right_btn)
+    TextView tvRightBtn;
+    @BindView(R.id.iv_caidan)
+    ImageView ivCaidan;
+    @BindView(R.id.title_view_heng)
+    View titleViewHeng;
+    @BindView(R.id.rl_title)
+    RelativeLayout rlTitle;
+    @BindView(R.id.et_money)
+    EditText etMoney;
+    @BindView(R.id.btn_dashang)
+    TextView btnDashang;
 
     @Override
     protected int initContentView() {
@@ -21,6 +49,7 @@ public class DaShangActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        ButterKnife.bind(this);
 
     }
 
@@ -31,6 +60,20 @@ public class DaShangActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        EditInputFilter editInputFilter = new EditInputFilter();
+        InputFilter[] filters = {editInputFilter};
+        etMoney.setFilters(filters);
+    }
 
+
+    @OnClick({R.id.ll_common_back, R.id.btn_dashang})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_common_back:
+                finish();
+                break;
+            case R.id.btn_dashang:
+                break;
+        }
     }
 }
