9b0bae5bfeafe97365857c0180b8c3c205a66a55
zhanglei <18346012117@163.com>
2022-09-15 10:06:51 +0800

    部分修改

---
+++ app/src/main/java/com/xueyiche/zjyk/xueyiche/examtext/bean/PhoneUserIdBean.java
@@ -0,0 +1,287 @@
+package com.xueyiche.zjyk.xueyiche.examtext.bean;
+
+public class PhoneUserIdBean {
+
+    private int code;
+    private String msg;
+    private String time;
+    private DataDTO data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public DataDTO getData() {
+        return data;
+    }
+
+    public void setData(DataDTO data) {
+        this.data = data;
+    }
+
+    public static class DataDTO {
+        private String user_id;
+        private String user_phone;
+        private String user_name;
+        private String head_img;
+        private String sex;
+        private String user_cards;
+        private String driver_cards;
+        private Object province_id;
+        private Object area_id;
+        private Object county_id;
+        private Object privince_name;
+        private Object area_name;
+        private Object county_name;
+        private String nickname;
+        private Object set_up_time;
+        private String equipment_type;
+        private String updat_time;
+        private Object verifycode;
+        private String address;
+        private String first_type;
+        private Object car_type;
+        private Object bind_card;
+        private Object bind_type;
+        private String user_random;
+        private String reg_system_time;
+        private String have_driver_school;
+        private String agent_no_off;
+
+        public String getUser_id() {
+            return user_id;
+        }
+
+        public void setUser_id(String user_id) {
+            this.user_id = user_id;
+        }
+
+        public String getUser_phone() {
+            return user_phone;
+        }
+
+        public void setUser_phone(String user_phone) {
+            this.user_phone = user_phone;
+        }
+
+        public String getUser_name() {
+            return user_name;
+        }
+
+        public void setUser_name(String user_name) {
+            this.user_name = user_name;
+        }
+
+        public String getHead_img() {
+            return head_img;
+        }
+
+        public void setHead_img(String head_img) {
+            this.head_img = head_img;
+        }
+
+        public String getSex() {
+            return sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+
+        public String getUser_cards() {
+            return user_cards;
+        }
+
+        public void setUser_cards(String user_cards) {
+            this.user_cards = user_cards;
+        }
+
+        public String getDriver_cards() {
+            return driver_cards;
+        }
+
+        public void setDriver_cards(String driver_cards) {
+            this.driver_cards = driver_cards;
+        }
+
+        public Object getProvince_id() {
+            return province_id;
+        }
+
+        public void setProvince_id(Object province_id) {
+            this.province_id = province_id;
+        }
+
+        public Object getArea_id() {
+            return area_id;
+        }
+
+        public void setArea_id(Object area_id) {
+            this.area_id = area_id;
+        }
+
+        public Object getCounty_id() {
+            return county_id;
+        }
+
+        public void setCounty_id(Object county_id) {
+            this.county_id = county_id;
+        }
+
+        public Object getPrivince_name() {
+            return privince_name;
+        }
+
+        public void setPrivince_name(Object privince_name) {
+            this.privince_name = privince_name;
+        }
+
+        public Object getArea_name() {
+            return area_name;
+        }
+
+        public void setArea_name(Object area_name) {
+            this.area_name = area_name;
+        }
+
+        public Object getCounty_name() {
+            return county_name;
+        }
+
+        public void setCounty_name(Object county_name) {
+            this.county_name = county_name;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+
+        public Object getSet_up_time() {
+            return set_up_time;
+        }
+
+        public void setSet_up_time(Object set_up_time) {
+            this.set_up_time = set_up_time;
+        }
+
+        public String getEquipment_type() {
+            return equipment_type;
+        }
+
+        public void setEquipment_type(String equipment_type) {
+            this.equipment_type = equipment_type;
+        }
+
+        public String getUpdat_time() {
+            return updat_time;
+        }
+
+        public void setUpdat_time(String updat_time) {
+            this.updat_time = updat_time;
+        }
+
+        public Object getVerifycode() {
+            return verifycode;
+        }
+
+        public void setVerifycode(Object verifycode) {
+            this.verifycode = verifycode;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public String getFirst_type() {
+            return first_type;
+        }
+
+        public void setFirst_type(String first_type) {
+            this.first_type = first_type;
+        }
+
+        public Object getCar_type() {
+            return car_type;
+        }
+
+        public void setCar_type(Object car_type) {
+            this.car_type = car_type;
+        }
+
+        public Object getBind_card() {
+            return bind_card;
+        }
+
+        public void setBind_card(Object bind_card) {
+            this.bind_card = bind_card;
+        }
+
+        public Object getBind_type() {
+            return bind_type;
+        }
+
+        public void setBind_type(Object bind_type) {
+            this.bind_type = bind_type;
+        }
+
+        public String getUser_random() {
+            return user_random;
+        }
+
+        public void setUser_random(String user_random) {
+            this.user_random = user_random;
+        }
+
+        public String getReg_system_time() {
+            return reg_system_time;
+        }
+
+        public void setReg_system_time(String reg_system_time) {
+            this.reg_system_time = reg_system_time;
+        }
+
+        public String getHave_driver_school() {
+            return have_driver_school;
+        }
+
+        public void setHave_driver_school(String have_driver_school) {
+            this.have_driver_school = have_driver_school;
+        }
+
+        public String getAgent_no_off() {
+            return agent_no_off;
+        }
+
+        public void setAgent_no_off(String agent_no_off) {
+            this.agent_no_off = agent_no_off;
+        }
+    }
+}
